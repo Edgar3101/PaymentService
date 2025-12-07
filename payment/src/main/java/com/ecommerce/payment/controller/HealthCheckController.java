5eabb875eff31278327dd33bce36c6e24dc87eb1
Edgar <edgar.hernandez3101@gmail.com>
2025-12-07 14:42:20 -0500

    FEATURE | Health Check | Added HealthCheckController for service health monitoring

---
+++ payment/src/main/java/com/ecommerce/payment/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.ecommerce.payment.controller;
+
+
+import jakarta.persistence.Entity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping()
+    public String healthCheck() {
+        return "Payment Service is up and running!";
+    }
+}
